e5a5e81deca08966974faf7c978a8eb62c4d253f
shimeming <shimeming@gmail.com>
2024-10-29 23:43:44 +0800

    feat(character): add jump buffering for improved input responsiveness

--- Assets/NTU-GP-2024-Fall/Scripts/Character/CharacterJump.cs
+++ Assets/NTU-GP-2024-Fall/Scripts/Character/CharacterJump.cs
@@ -13,9 +13,14 @@ public class CharacterJump : MonoBehaviour {
     [SerializeField] float jumpHangTimeThresholdSpeed = 0.5f;
     [Tooltip("Duration the character can still jump after leaving the ground.")]
     [SerializeField] float coyoteTime = 0.1f;
-    float coyoteTimeCounter;
+    [Tooltip("Time buffer to allow jumps even if the jump button is pressed slightly early, "
+         + "ensuring intended jumps are more likely to succeed.")]
+    [SerializeField] float jumpBufferTime = 0.1f;
 
     float jumpPower;
+
+    float coyoteTimeCounter;
+    float jumpBufferCounter;
 
     [SerializeField] AudioClip jumpSound;
     AudioSource audioSource;
@@ -38,15 +43,22 @@ public class CharacterJump : MonoBehaviour {
         }
         if (!character.CurrentMovement.IsJumpEnabled) return;
 
-        if (Input.GetButtonDown("Jump") && coyoteTimeCounter > 0f) {
+        if (Input.GetButtonDown("Jump")) {
+            jumpBufferCounter = jumpBufferTime;
+        } else {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f) {
             character.Rb.velocity = new Vector2(character.Rb.velocity.x, jumpPower);
             character.Rb.gravityScale = character.NormalGravityScale * slowerFallGravityMultiplier;
             if (jumpSound != null) audioSource.PlayOneShot(jumpSound);
+            jumpBufferCounter = 0f;
             coyoteTimeCounter = 0f;
         }
         if (Input.GetButtonUp("Jump") && character.Rb.velocity.y > 0) {
             character.Rb.gravityScale = character.NormalGravityScale;
-            character.Rb.velocity = new Vector2(character.Rb.velocity.x, 
+            character.Rb.velocity = new Vector2(character.Rb.velocity.x,
                 releaseJumpSpeedMultiplier * character.Rb.velocity.y);
         }
 

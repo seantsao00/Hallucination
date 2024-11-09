1f2fdd78931cdc73424b6d844a0cd3280f2683e5
shimeming <shimeming@gmail.com>
2024-11-10 02:58:51 +0800

    feat(sync): update sync to use world space coordinates and sync movable platforms

--- Assets/NTU-GP-2024-Fall/Scripts/World/Syncable.cs
+++ Assets/NTU-GP-2024-Fall/Scripts/World/Syncable.cs
@@ -3,6 +3,12 @@ using UnityEditor;
 
 public class Syncable : MonoBehaviour {
     public GameObject SyncedObject;
+    GameObject currentWorld, syncedWorld;
+
+    void Start() {
+        currentWorld = CurrentWorld();
+        syncedWorld = SyncedWorld();
+    }
 
     protected void OnEnable() {
         WorldSwitchManager.Instance.OnWorldSwitch.AddListener(SyncState);
@@ -12,30 +18,43 @@ public class Syncable : MonoBehaviour {
         WorldSwitchManager.Instance.OnWorldSwitch.RemoveListener(SyncState);
     }
 
-    protected virtual Vector3 SyncedLocalPosition() {
-        return transform.localPosition;
+    protected Vector3 CurrentWorldLocalPosition() {
+        return currentWorld.transform.InverseTransformPoint(transform.position);
+    }
+
+    protected virtual Vector3 SyncedPosition() {
+        Vector3 currentWorldPosition = CurrentWorldLocalPosition();
+        return syncedWorld.transform.TransformPoint(currentWorldPosition);
     }
 
     public virtual void SyncState() {
-        SyncedObject.transform.localPosition = SyncedLocalPosition();
+        SyncedObject.transform.position = SyncedPosition();
     }
 
     void OnDrawGizmosSelected() {
         if (SyncedObject == null || WorldSwitchManager.Instance?.WorldFairyEnvironment == null) return;
-
-        GameObject fairyWorld = WorldSwitchManager.Instance.WorldFairyEnvironment;
-        GameObject bearWorld = WorldSwitchManager.Instance.WorldBearEnvironment;
-        GameObject syncedWorld = transform.IsChildOf(fairyWorld.transform) ? bearWorld : fairyWorld;
-
-        Vector3 syncedLocalPosition = SyncedLocalPosition();
+        currentWorld = CurrentWorld();
+        syncedWorld = SyncedWorld();
 
         // Draw a rim around the position in WorldFairyEnvironment's local space
         Gizmos.color = Color.cyan;
-        Vector3 syncedPosition = syncedWorld.transform.TransformPoint(syncedLocalPosition);
+        Vector3 syncedPosition = SyncedPosition();
         
         // Draw a wire sphere at this position
         Gizmos.DrawWireSphere(syncedPosition, 0.3f);
         // Gizmos.DrawIcon(syncedPosition, "sv_icon_dot14_sml");
         Handles.Label(syncedPosition, "synced position");
+    }
+
+    protected GameObject CurrentWorld() {
+        GameObject fairyWorld = WorldSwitchManager.Instance.WorldFairyEnvironment;
+        GameObject bearWorld = WorldSwitchManager.Instance.WorldBearEnvironment;
+        return transform.IsChildOf(fairyWorld.transform) ? fairyWorld : bearWorld;
+    }
+
+    protected GameObject SyncedWorld() {
+        GameObject fairyWorld = WorldSwitchManager.Instance.WorldFairyEnvironment;
+        GameObject bearWorld = WorldSwitchManager.Instance.WorldBearEnvironment;
+        return transform.IsChildOf(bearWorld.transform) ? fairyWorld : bearWorld;
     }
 }

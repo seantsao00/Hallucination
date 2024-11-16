47be88caf614e6dec7ead81ccba0708001e15876
shimeming <shimeming@gmail.com>
2024-11-16 08:23:08 +0800

    feat(tool): improve outline generator's interface

--- Assets/NTU-GP-2024-Fall/Scripts/EditorTools/GenerateTilemapOutline.cs
+++ Assets/NTU-GP-2024-Fall/Scripts/EditorTools/GenerateTilemapOutline.cs
@@ -14,44 +14,43 @@ public class GenerateTilemapOutlineInspector : Editor {
 }
 
 public class GenerateTilemapOutline : MonoBehaviour {
-    [SerializeField] Tilemap sourceTilemap;
-    [SerializeField] Tilemap outlineTilemap;
+    [SerializeField] Tilemap[] sourceTilemaps;
     [SerializeField] Tile outlineTile;
-    [SerializeField] Color outlineColor;
+    [SerializeField] Color outlineColor = Color.black;
     [SerializeField] string outlineName = "Outline";
 
     public void GenerateOutline() {
-        sourceTilemap.CompressBounds();
-        if (outlineTilemap == null) {
+        foreach (var sourceTilemap in sourceTilemaps) {
+            Tilemap outlineTilemap;
+            sourceTilemap.CompressBounds();
             GameObject outlineTilemapGameObject = new GameObject();
             outlineTilemapGameObject.name = outlineName;
             outlineTilemapGameObject.transform.position = sourceTilemap.transform.position;
             outlineTilemapGameObject.transform.parent = sourceTilemap.transform;
             outlineTilemap = outlineTilemapGameObject.AddComponent<Tilemap>();
             outlineTilemapGameObject.AddComponent<TilemapRenderer>();
-        }
-        // outlineTilemap.ClearAllTiles();
-        outlineTilemap.color = outlineColor;
-        BoundsInt bounds = sourceTilemap.cellBounds;
+            // outlineTilemap.ClearAllTiles();
+            outlineTilemap.color = outlineColor;
+            BoundsInt bounds = sourceTilemap.cellBounds;
 
-        for (int x = bounds.xMin; x < bounds.xMax; x++) {
-            for (int y = bounds.yMin; y < bounds.yMax; y++) {
-                Vector3Int position = new Vector3Int(x, y, 0);
+            for (int x = bounds.xMin; x < bounds.xMax; x++) {
+                for (int y = bounds.yMin; y < bounds.yMax; y++) {
+                    Vector3Int position = new Vector3Int(x, y, 0);
 
-                TileBase tile = sourceTilemap.GetTile(position);
-                if (tile != null) {
-                    // Copy tile to the target tilemap
-                    outlineTilemap.SetTile(position, outlineTile);
-                    // Debug.Log($"{position}({x}, {y}): {tile != null}");
+                    TileBase tile = sourceTilemap.GetTile(position);
+                    if (tile != null) {
+                        // Copy tile to the target tilemap
+                        outlineTilemap.SetTile(position, outlineTile);
+                        // Debug.Log($"{position}({x}, {y}): {tile != null}");
 
-                    // Optionally, copy other tile data (like color or transform)
-                    // outlineTilemap.SetTransformMatrix(position, sourceTilemap.GetTransformMatrix(position));
-                    // outlineTilemap.SetTileFlags(position, sourceTilemap.GetTileFlags(position));
-                    // outlineTilemap.SetColor(position, sourceTilemap.GetColor(position));
+                        // Optionally, copy other tile data (like color or transform)
+                        // outlineTilemap.SetTransformMatrix(position, sourceTilemap.GetTransformMatrix(position));
+                        // outlineTilemap.SetTileFlags(position, sourceTilemap.GetTileFlags(position));
+                        // outlineTilemap.SetColor(position, sourceTilemap.GetColor(position));
+                    }
                 }
             }
+            outlineTilemap.RefreshAllTiles();
         }
-
-        outlineTilemap.RefreshAllTiles();
     }
 }

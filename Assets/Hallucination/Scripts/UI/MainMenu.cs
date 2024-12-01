18f499a90ddf87994fd6c84171d3056b71e9f089
seantsao00 <seantsao00@gmail.com>
2024-12-02 02:23:48 +0800

    chore: remove unused scripts

--- Assets/Hallucination/Scripts/UI/MainMenu.cs
+++ Assets/Hallucination/Scripts/UI/MainMenu.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public SceneFader sceneFader;
     AsyncOperation loadingOperation;
     void Start() {
         loadingOperation = SceneManager.LoadSceneAsync("Intro");

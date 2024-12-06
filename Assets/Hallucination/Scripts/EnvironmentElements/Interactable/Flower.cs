0914505f17bc590dd15a3afe6b07370c11327e3b
shimeming <shimeming@gmail.com>
2024-12-07 07:49:45 +0800

    feat(projection): change ground's collider type to polygons

--- Assets/Hallucination/Scripts/EnvironmentElements/Interactable/Flower.cs
+++ Assets/Hallucination/Scripts/EnvironmentElements/Interactable/Flower.cs
@@ -10,6 +10,7 @@ public class Flower : MonoBehaviour {
     public float activateDuration = 3f;
     [SerializeField] CapturedSurroundings capturedSurroundings;
     bool isFlowerActivated = false;
+    float targetAlpha = 0.8f;
 
     void Awake() {
         detector = WorldSwitchManager.Instance.Bear.GetComponentInChildren<CharacterProjectionDetector>();
@@ -58,28 +59,23 @@ public class Flower : MonoBehaviour {
             Vector2 newPosition = (Vector2)projection.transform.position + offset;
             Transform parent = transform.parent;
             duplicatedObject = Instantiate(projection, newPosition, projection.transform.rotation, parent);
-            RecoverCollisionLayer(duplicatedObject);
             if (duplicatedObject.GetComponent<Tilemap>() != null) {
                 TrimTilemap(detector.transform.position, detector.radius,
                     projection.GetComponent<Tilemap>(), duplicatedObject.GetComponent<Tilemap>());
             } else {
                 var spriteRenderer = duplicatedObject.GetComponent<SpriteRenderer>();
                 if (spriteRenderer != null) {
-                    spriteRenderer.color = new Color(1, 1, 1, 1f);
+                    spriteRenderer.color = new Color(1, 1, 1, targetAlpha);
                 }
             }
+            RecoverCollisionLayer(duplicatedObject);
             duplicatedObject.name = projection.name + "_Copy";
-            if (projection.layer == LayerMask.NameToLayer("ProjectionGround")) {
-                duplicatedObject.layer = LayerMask.NameToLayer("Ground");
-            } else if (projection.layer == LayerMask.NameToLayer("FairyProjection")) {
-                duplicatedObject.layer = LayerMask.NameToLayer("Default");
-            }
             duplicatedObjects.Add(duplicatedObject);
         }
     }
 
     void TrimTilemap(Vector2 center, float radius, Tilemap sourceTilemap, Tilemap duplicatedTilemap) {
-        duplicatedTilemap.color = new Color(1, 1, 1, 1f);
+        duplicatedTilemap.color = new Color(1, 1, 1, targetAlpha);
         BoundsInt bounds = sourceTilemap.cellBounds;
         for (int x = bounds.xMin; x < bounds.xMax; x++) {
             for (int y = bounds.yMin; y < bounds.yMax; y++) {
@@ -94,29 +90,20 @@ public class Flower : MonoBehaviour {
                 }
             }
         }
-
-        // BoundsInt bounds = new BoundsInt(
-        //     sourceTilemap.WorldToCell(center),
-        //     new Vector3Int(Mathf.CeilToInt(radius * 2) + 1, Mathf.CeilToInt(radius * 2) + 1)
-        // );
-        // for (int x = bounds.xMin; x <= bounds.xMax; x++) {
-        //     for (int y = bounds.yMin; y <= bounds.yMax; y++) {
-        //         Vector3Int tilePos = new Vector3Int(x, y, 0);
-        //         Vector3 tileWorldPos = sourceTilemap.CellToWorld(tilePos) + new Vector3(0.5f, 0.5f, 0);
-        //         if (Vector3.Distance(tileWorldPos, center) <= radius) {
-        //             TileBase tile = sourceTilemap.GetTile(tilePos);
-        //             if (tile != null) {
-        //                 duplicatedTilemap.SetTile(tilePos, tile);
-        //             }
-        //         }
-        //     }
-        // }
         duplicatedTilemap.RefreshAllTiles();
     }
 
     void RecoverCollisionLayer(GameObject obj) {
         Collider2D[] colliders = obj.GetComponents<Collider2D>();
-        foreach (var collider in colliders) collider.excludeLayers = 0;
+        if (obj.layer == LayerMask.NameToLayer("ProjectionGround")) {
+            obj.layer = LayerMask.NameToLayer("Ground");
+        } else if (obj.layer == LayerMask.NameToLayer("FairyProjection")) {
+            obj.layer = LayerMask.NameToLayer("Default");
+        }
+        Vector2 fairyPosition = WorldSwitchManager.Instance.Fairy.transform.position;
+        foreach (var collider in colliders) {
+            collider.excludeLayers = 0;
+        }
     }
     void DestroyProjectionObjects() {
         // print(duplicatedObjects);

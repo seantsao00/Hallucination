74fc1a75ae4a5b6aa3c16c1b51ac92c30bfe4271
shimeming <shimeming@gmail.com>
2024-12-08 20:05:47 +0800

    feat(key): add inspector support of key's color

--- Assets/Hallucination/Scripts/EnvironmentElements/Key.cs
+++ Assets/Hallucination/Scripts/EnvironmentElements/Key.cs
@@ -5,6 +5,7 @@ public class Key : MonoBehaviour {
 
     public delegate void UnlockHandler(Key key);
     public event UnlockHandler Unlock;
+    [SerializeField] Color eatenColor = Color.green;
 
     void OnTriggerEnter2D(Collider2D collision) {
         if (isCollected) return;
@@ -17,6 +18,6 @@ public class Key : MonoBehaviour {
         isCollected = true;
         Unlock?.Invoke(this);
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = new Color(0f, 255f, 0f, 1f);
+        spriteRenderer.color = eatenColor;
     }
 }
